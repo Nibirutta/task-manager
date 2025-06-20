f2748bc2ec4557c195aefc0ec0a716931ce9a7df
Lucas <lucas.s.augusto@hotmail.com>
2025-06-20 08:37:11 -0300

    Sidebar is completed

---
+++ DEVLOG.md
@@ -0,0 +1,5 @@
+**Continuing my taskproject, already done with the api, now it's front end time**
+
+- *Created a basic project structure, with some HTML,JS & CSS files.*
+- *Started building a simple sidebar, to ensure a clean navigation throughout the website.*
+- *Sidebar is completed; now I'll build the main content.*
---
+++ js/main.js
@@ -0,0 +1,11 @@
+const sidebarBtn = document.getElementById('sidebar-btn');
+const sidebar = document.getElementById('sidebar');
+
+
+sidebarBtn.addEventListener('click', () => {
+    if (sidebar.classList.contains('expanded')) {
+        sidebar.classList.remove('expanded');
+    } else {
+        sidebar.classList.add('expanded');
+    }
+});
