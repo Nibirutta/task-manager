84b68b26d6668a071c4062397698020b6fa249b9
Lucino Campos <lucinogabriel1510@gmail.com>
2025-08-08 17:58:17 -0300

    :sparkles: completed logic for loginform component

--- src/features/LoginForm/LoginForm.tsx
+++ src/features/LoginForm/LoginForm.tsx
@@ -1,63 +1,71 @@
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
-import { Mail, Lock, Send } from 'lucide-react';
-import style from './LoginForm.module.css'
-import InputField from '../../components/InputField/InputField';
-import SubmitBtn from '../../components/SubmitBtn/SubmitBtn';
-import idGenerator from '../../utils/idGenerator';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Lock, Send, CircleUser } from "lucide-react";
+import style from "./LoginForm.module.css";
+import InputField from "../../components/InputField/InputField";
+import SubmitBtn from "../../components/SubmitBtn/SubmitBtn";
+import idGenerator from "../../utils/idGenerator";
+import sendLogin from "../../api/taskAPI/login";
 
 const loginSchema = z.object({
-  email: z.string().email({ message: 'Por favor, insira um e-mail válido.' }),
-  password: z.string().min(8, { message: 'A senha deve ter pelo menos 8 caracteres.' }),
+	username: z
+		.string({
+			error: "O nome de usuário é obrigatório.",
+		})
+		.trim()
+		.min(3, "O nome de usuário deve ter no mínimo 3 caracteres.")
+		.max(30, "O nome de usuário não pode ter mais de 30 caracteres.")
+		.regex(/^[a-zA-Z0-9_]+$/, "Use apenas letras, números e underscore (_)."),
+	password: z
+		.string({ error: "A senha é obrigatória." })
+		.min(8, "A senha deve ter no mínimo 8 caracteres.")
+		.regex(/[a-z]/, "A senha deve conter ao menos uma letra minúscula.")
+		.regex(/[A-Z]/, "A senha deve conter ao menos uma letra maiúscula.")
+		.regex(/[0-9]/, "A senha deve conter ao menos um número.")
+		.regex(/[^a-zA-Z0-9]/, "A senha deve conter ao menos um caractere especial."),
 });
 
 type LoginFormInputs = z.infer<typeof loginSchema>;
 
 function LoginForm() {
- 
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting, isValid: isFormValid },
   } = useForm<LoginFormInputs>({
     resolver: zodResolver(loginSchema),
-    mode: 'onTouched', 
+    mode: "onTouched",
   });
 
-  
   const onSubmit = async (data: LoginFormInputs) => {
-    console.log('Enviando dados para a API:', data);
-    // Simula uma chamada de API
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log('Dados enviados com sucesso!');
+    const token = await sendLogin(data);
+    console.log(token);
+
+    /* criar uma função que reseta o form */
   };
 
   return (
-	  <form 
-      onSubmit={handleSubmit(onSubmit)} 
-      className={style.form}
-	  >
+    <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
       <h2 className={style.title}>Login</h2>
-		
-      
+
       <InputField
         id={idGenerator()}
-        label="E-mail"
-        placeholder="seuemail@exemplo.com"
-        Icon={Mail}
-        {...register('email')}
-        isValid={!errors.email}
-        errorMessage={errors.email?.message}
+        label="Username"
+        placeholder="Seu nome de usuário"
+        Icon={CircleUser}
+        {...register("username")}
+        isValid={!errors.username}
+        errorMessage={errors.username?.message}
       />
 
       <InputField
         id={idGenerator()}
         label="Senha"
-        type="password" 
+        type="password"
         placeholder="••••••••"
         Icon={Lock}
-        {...register('password')}
+        {...register("password")}
         isValid={!errors.password}
         errorMessage={errors.password?.message}
       />
@@ -69,18 +77,16 @@ function LoginForm() {
         disabled={!isFormValid || isSubmitting}
       />
 
-	  <div className={style.redirect}>
-		<a href="http://" target="_blank" rel="noopener noreferrer">
-			<span>Não tem uma conta</span>
-		</a>
+      <div className={style.redirect}>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <span>Não tem uma conta</span>
+        </a>
 
-		<a href="http://" target="_blank" rel="noopener noreferrer">
-			<span>Esqueceu sua senha?</span>
-		</a>
-	  </div>
-
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <span>Esqueceu sua senha?</span>
+        </a>
+      </div>
     </form>
-
   );
 }
 
