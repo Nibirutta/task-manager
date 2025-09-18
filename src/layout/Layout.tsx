c4cc469608d0a09ac0bc168d43e5d3d6d8b42684
Lucino Campos <lucinogabriel1510@gmail.com>
2025-09-18 17:03:31 -0300

    :zap: delete some redundant imports that were affecting performance

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer/Footer';
 import style from './Layout.module.css';
-
-
-// import Navbar from '../../features/Navbar/Navbar'; // Futuramente, você irá descomentar e usar isso
+import Header from './Header/Header';
 
 /**
  * O componente de Layout define a estrutura visual comum da aplicação.
@@ -13,7 +11,7 @@ import style from './Layout.module.css';
 const Layout = () => {
   return (
     <div className={style.layout}>
-      {/* <Navbar /> */}
+      <Header />
       <main>
         <Outlet /> {/* O conteúdo da rota filha será renderizado aqui */}
       </main>
