a86998592d0724376be435042126fd17c0f5ff27
Lucino Campos <lucinogabriel1510@gmail.com>
2025-10-24 16:16:09 -0300

    :heavy_plus_sign: build - new dependency :  vite plugin basic ssl

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 import flowbiteReact from "flowbite-react/plugin/vite";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://vitejs.dev/config/
 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(), flowbiteReact()],
+  plugins: [react(), tailwindcss(), svgr(), flowbiteReact(), basicSsl()],
+  server: {
+    https: true, // Habilita o servidor HTTPS
+    host: true, // Permite acesso externo, útil para testar em outros dispositivos na rede
+    port: 3000, // Ou a porta que você preferir
+  },
+
 })
