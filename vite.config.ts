38250a23066c3609def5b6132f51c52b28a34882
Lucino Campos <lucinogabriel1510@gmail.com>
2025-10-24 16:32:10 -0300

    :arrow_down: rebuilding vite configs from before new dependency was implemented

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 import flowbiteReact from "flowbite-react/plugin/vite";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://vitejs.dev/config/
 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(), flowbiteReact(), basicSsl()],
-  server: {
-    https: true, // Habilita o servidor HTTPS
-    host: true, // Permite acesso externo, útil para testar em outros dispositivos na rede
-    port: 3000, // Ou a porta que você preferir
-  },
+  plugins: [react(), tailwindcss(), svgr(), flowbiteReact()],
 
 })
